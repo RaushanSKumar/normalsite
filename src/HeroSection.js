e0aaadfd5824625abfd3dc6137eeb7f1983cc498
unknown <raushankumar500009@gmail.com>
2022-03-05 21:54:05 +0530

    normal site

---
+++ src/HeroSection.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+
+const HeroSection = () => {
+    return(
+        <div className="jumbotron ">
+        <h1 className="display-4">App looks great!</h1>
+        <p className="lead">
+       Raushan checking paragraph using Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, fugit.
+        </p>
+        <hr className="my-4" />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+          incidunt?
+        </p>
+        <a className="btn btn-success btn-lg" href="#" role="button"
+          >React is awesome</a
+        >
+      </div>
+    );
+};
+
+
+
+export default HeroSection;
